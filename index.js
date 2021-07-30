dcaf68fd67919644372df4b1c73961c733598d25
Akm2021 <furqatiy30@gmail.com>
2021-07-30 23:36:43 +0500

    shuffle array

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log("Salom Dunyo...");
-conosle.log("Na gaplar...");
-console.log("Ishni boshlaymiz...");
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * arr.length);
+
+        [arr[i], arr[rand]] = [arr[rand], arr[i]];
+    }
+    return arr;
+}
+let result = shuffleArray(arr);
+console.log(result);
